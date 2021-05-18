21dbe33653510bc6bd87a85ea4c333152526dbcf
anuws505 <anuws505@ais900.org>
2021-05-18 16:25:36 +0700

    initial fonts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './pages/index/index.component';
+import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: 'name', component: NameComponent
+  //   children: [
+  //     { path: '', component: NameComponent },
+  //     { path: ':path', component: NameComponent },
+  //     { path: ':path/:params', component: NameComponent }
+  //   ],
+  //   canActivate: [AuthenguardGuard]
+  // },
+  { path: '', component: IndexComponent },
+  // { path: '', pathMatch: 'full', redirectTo: 'main' },
+  { path: '**', component: PagenotfoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
