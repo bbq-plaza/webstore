61a9227ab08df3f3ce4d821f2cf3a57333343e12
anuws505 <anuws505@ais900.org>
2021-06-19 02:54:30 +0700

    modify display data

--- src/app/pages/components/product-item/product-item.component.ts
+++ src/app/pages/components/product-item/product-item.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductItemComponent implements OnInit {
 
+  public productItem: any = {
+    sku: 101,
+    featured: '', // item-hot
+    badge: [
+      { class: 'hot', label: 'hot' },
+      { class: 'off', label: 'off 999%' }
+    ],
+    title: 'Product Name ไอเทมชั้นหนึ่ง สุดร้อนแรงแห่งปี ไม่ซื้อไม่ได้แล้ว',
+    imagePath: '/assets/example/example.png',
+    priceTotal: 999999999,
+    subTotal: 999999999
+  };
+
   constructor() { }
 
   ngOnInit(): void {
